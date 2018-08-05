1ae70e7b83a5951a4dec1155f2b2d9fd15bfb7ea
cystandout <2936236899@qq.com>
2018-08-05 17:21:14 +0800

    代码提交
    代码提交

--- server/db/db.js
+++
@@ -1,8 +0,0 @@
-// 数据库连接配置
-module.exports = {
-		host: 'localhost',
-		user: 'root',
-		password: 'root',
-		database: 'market',
-		port: '3306'
-}
--- src/token/checkToken.js
+++
@@ -1,6 +0,0 @@
-const jwt = require('jsonwebtoken');
-module.exports=function(token,screct){
-// get the decoded payload and header
-const decoded = jwt.verify(token,screct);
-return decoded
-}
